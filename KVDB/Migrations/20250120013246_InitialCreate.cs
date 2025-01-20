efc04749a30d14ccc1e234ae4afd234a0ffe95e2
Serkan <serkanbay02@hotmail.com>
2025-01-20 04:15:15 +0100

    Save Episode and Transcript Model data to Database

---
+++ KVDB/Migrations/20250120013246_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KVDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Episode",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YoutubeId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Episode", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transcript",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Start = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Duration = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    EpisodeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transcript", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transcript_Episode_EpisodeId",
+                        column: x => x.EpisodeId,
+                        principalTable: "Episode",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transcript_EpisodeId",
+                table: "Transcript",
+                column: "EpisodeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transcript");
+
+            migrationBuilder.DropTable(
+                name: "Episode");
+        }
+    }
+}
--- KVDB/Models/Transcript.cs
+++ KVDB/Models/Transcript.cs
@@ -12,7 +12,7 @@ namespace KVDB.Models
 
         public int EpisodeId { get; set; }
         //This is the foreign key field.It stores the ID of the related Episode entity in the database.
-        public virtual Episode Episode { get; set; }
+        public required virtual Episode Episode { get; set; }
         //This is the navigation property.It provides a way to navigate from a Transcript entity to its associated Episode entity.
     }
 }
